d04607ec676867aec0f88f06475b71223d395811
Mario <mario@mariogl.com>
2021-01-13 17:50:22 +0100

    Resuelto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const anchoAltoCelda = 100;
+  let casillas = [];
+  for (let x = 1; x <= 3; x++) {
+    for (let y = 1; y <= 3; y++) {
+      casillas.push({
+        x,
+        y
+      });
+    }
+  }
+  const [rotacion, setRotacion] = useState(0);
+  const [posicionActual, setPosicionActual] = useState({ x: 1, y: 1 });
+  const [mostrarAviso, setMostrarAviso] = useState(false);
+  const moverACasilla = casilla => {
+    girarPizza();
+    setPosicionActual(casilla);
+  };
+  const estaFuera = casilla => casilla.x < 1 || casilla.x > 3 || casilla.y < 1 || casilla.y > 3;
+  const moverPizzaArriba = () => {
+    const nuevaCasilla = { ...posicionActual, x: posicionActual.x - 1 };
+    if (estaFuera(nuevaCasilla)) {
+      return;
+    }
+    girarPizza();
+    setPosicionActual(nuevaCasilla);
+  };
+  const muestraAviso = () => {
+    setMostrarAviso(true);
+    setTimeout(() => {
+      setMostrarAviso(false);
+    }, 2000);
+  };
+  const moverPizzaAbajo = () => {
+    const nuevaCasilla = { ...posicionActual, x: posicionActual.x + 1 };
+    if (estaFuera(nuevaCasilla)) {
+      muestraAviso();
+      return;
+    }
+    girarPizza();
+    setPosicionActual(nuevaCasilla);
+  };
+  const moverPizzaDerecha = () => {
+    const nuevaCasilla = { ...posicionActual, y: posicionActual.y + 1 };
+    if (estaFuera(nuevaCasilla)) {
+      muestraAviso();
+      return;
+    }
+    girarPizza();
+    setPosicionActual(nuevaCasilla);
+  };
+  const moverPizzaIzquierda = () => {
+    const nuevaCasilla = { ...posicionActual, y: posicionActual.y - 1 };
+    if (estaFuera(nuevaCasilla)) {
+      muestraAviso();
+      return;
+    }
+    girarPizza();
+    setPosicionActual(nuevaCasilla);
+  };
+  const girarPizza = () => {
+    setRotacion((rotacion + 1) % 4);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={`aviso${mostrarAviso ? " on" : ""}`}>¡Eh, que te sales!</div>
+      <ul className="controles">
+        <li onClick={moverPizzaArriba}>⬆</li>
+        <li onClick={moverPizzaAbajo}>⬇</li>
+        <li onClick={moverPizzaIzquierda}>⬅</li>
+        <li onClick={moverPizzaDerecha}>➡</li>
+      </ul>
+      <div className="info">
+        Casilla: {posicionActual.x}, {posicionActual.y}
+      </div>
+      <div className="tablero-contenedor">
+        <ul className="tablero">
+          {
+            casillas.map(casilla =>
+              <li
+                onClick={() => moverACasilla({ x: casilla.x, y: casilla.y })}
+                key={`${casilla.x}-${casilla.y}`}
+              ></li>
+            )
+          }
+        </ul>
+        <div className={`ficha rotacion${rotacion}`} style={{
+          top: posicionActual.x * anchoAltoCelda - (anchoAltoCelda / 2),
+          left: posicionActual.y * anchoAltoCelda - (anchoAltoCelda / 2)
+        }}>🍕</div>
+      </div>
+    </>
   );
 }
 
